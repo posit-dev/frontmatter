518f4104d1fe072882be61e79501a9ccccb1c318
Garrick Aden-Buie <garrick@adenbuie.com>
2026-01-09 10:27:24 -0500

    ⌦ avoid withr dependency

--- tests/testthat/test-default_parsers.R
+++ tests/testthat/test-default_parsers.R
@@ -4,13 +4,9 @@ test_that("YAML 1.1 option uses yaml package", {
   # YAML 1.1 treats 'yes' as boolean TRUE, YAML 1.2 treats it as string "yes"
   text <- "---\nvalue: yes\n---\nBody"
 
-  withr::with_options(
-    list(frontmatter.parse_yaml.spec = "1.1"),
-    {
-      result <- parse_front_matter(text)
-      expect_true(result$data$value)
-    }
-  )
+  rlang::local_options(frontmatter.parse_yaml.spec = "1.1")
+  result <- parse_front_matter(text)
+  expect_true(result$data$value)
 })
 
 test_that("YAML 1.2 option uses yaml12 package", {
@@ -19,13 +15,9 @@ test_that("YAML 1.2 option uses yaml12 package", {
   # YAML 1.2 treats 'yes' as string "yes"
   text <- "---\nvalue: yes\n---\nBody"
 
-  withr::with_options(
-    list(frontmatter.parse_yaml.spec = "1.2"),
-    {
-      result <- parse_front_matter(text)
-      expect_equal(result$data$value, "yes")
-    }
-  )
+  rlang::local_options(frontmatter.parse_yaml.spec = "1.2")
+  result <- parse_front_matter(text)
+  expect_equal(result$data$value, "yes")
 })
 
 test_that("YAML spec envvar works when option not set", {
@@ -33,16 +25,23 @@ test_that("YAML spec envvar works when option not set", {
 
   text <- "---\nvalue: yes\n---\nBody"
 
-  withr::with_options(
-    list(frontmatter.parse_yaml.spec = NULL),
-    withr::with_envvar(
-      c(FRONTMATTER_PARSE_YAML_SPEC = "1.1"),
-      {
-        result <- parse_front_matter(text)
-        expect_true(result$data$value)
+  rlang::local_options(frontmatter.parse_yaml.spec = NULL)
+
+  old_env <- Sys.getenv("FRONTMATTER_PARSE_YAML_SPEC", unset = NA)
+  Sys.setenv(FRONTMATTER_PARSE_YAML_SPEC = "1.1")
+  on.exit(
+    {
+      if (is.na(old_env)) {
+        Sys.unsetenv("FRONTMATTER_PARSE_YAML_SPEC")
+      } else {
+        Sys.setenv(FRONTMATTER_PARSE_YAML_SPEC = old_env)
       }
-    )
+    },
+    add = TRUE
   )
+
+  result <- parse_front_matter(text)
+  expect_true(result$data$value)
 })
 
 test_that("YAML spec option takes precedence over envvar", {
@@ -52,25 +51,28 @@ test_that("YAML spec option takes precedence over envvar", {
   text <- "---\nvalue: yes\n---\nBody"
 
   # Option set to 1.2, envvar set to 1.1 - option should win
-  withr::with_options(
-    list(frontmatter.parse_yaml.spec = "1.2"),
-    withr::with_envvar(
-      c(FRONTMATTER_PARSE_YAML_SPEC = "1.1"),
-      {
-        result <- parse_front_matter(text)
-        expect_equal(result$data$value, "yes")
+  rlang::local_options(frontmatter.parse_yaml.spec = "1.2")
+
+  old_env <- Sys.getenv("FRONTMATTER_PARSE_YAML_SPEC", unset = NA)
+  Sys.setenv(FRONTMATTER_PARSE_YAML_SPEC = "1.1")
+  on.exit(
+    {
+      if (is.na(old_env)) {
+        Sys.unsetenv("FRONTMATTER_PARSE_YAML_SPEC")
+      } else {
+        Sys.setenv(FRONTMATTER_PARSE_YAML_SPEC = old_env)
       }
-    )
+    },
+    add = TRUE
   )
+
+  result <- parse_front_matter(text)
+  expect_equal(result$data$value, "yes")
 })
 
 test_that("invalid YAML spec value errors", {
   text <- "---\nvalue: test\n---\nBody"
 
-  withr::with_options(
-    list(frontmatter.parse_yaml.spec = "invalid"),
-    {
-      expect_error(parse_front_matter(text), "1.1.*1.2")
-    }
-  )
+  rlang::local_options(frontmatter.parse_yaml.spec = "invalid")
+  expect_error(parse_front_matter(text), "1.1.*1.2")
 })
